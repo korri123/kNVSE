korri123/kNVSE

commit 5f178f5472404344bf3f20b111cd0fd143c89753

Use shared_ptr for g_timeTrackedAnims

nvse_plugin_example/main.cpp
@@ -87,14 +87,15 @@ void HandleAnimTimes()
 	{
 		return !actor || actor->IsDying(true) || actor->IsDeleted() || !actor->baseProcess;
 	};
-	for (auto iter = g_timeTrackedAnims.begin(); iter != g_timeTrackedAnims.end();)
+	const auto timeTrackedAnims = std::map(g_timeTrackedAnims);
+	for (auto iter = timeTrackedAnims.begin(); iter != timeTrackedAnims.end(); ++iter)
 	{
 		const auto erase = [&]()
 		{
-			iter = g_timeTrackedAnims.erase(iter);
+			g_timeTrackedAnims.erase(iter);
 		};
 
-		auto& animTime = iter->second;
+		auto& animTime = *iter->second;
 		auto* anim = animTime.anim;
 		if (!anim)
 		{
@@ -197,7 +198,6 @@ void HandleAnimTimes()
 		{
 			animTime.scriptLines.Update(time, animData, _L(Script* script, ThisStdCall(0x5AC1E0, script, actor, actor->GetEventList(), nullptr, false)));
 		}
-		++iter;
 	}
 
 
@@ -672,6 +672,7 @@ void MessageHandler(NVSEMessagingInterface::Message* msg)
 		g_timeTrackedGroups.clear();
 	}
 }
+
 
 bool NVSEPlugin_Query(const NVSEInterface* nvse, PluginInfo* info)
 {

nvse_plugin_example/commands_animation.cpp
@@ -338,7 +338,7 @@ AnimPath* HandleAimUpDownRandomness(UInt32 animGroupId, SavedAnims& anims)
 
 std::list<BurstFireData> g_burstFireQueue;
 
-std::map<BSAnimGroupSequence*, AnimTime> g_timeTrackedAnims;
+std::map<BSAnimGroupSequence*, std::shared_ptr<AnimTime>> g_timeTrackedAnims;
 
 
 std::map<ActorSequenceKey, SavedAnimsTime> g_timeTrackedGroups;
@@ -379,10 +379,8 @@ bool HandleExtraOperations(AnimData* animData, BSAnimGroupSequence* anim, AnimPa
 	{
 		if (!animTimePtr)
 		{
-			auto iter = g_timeTrackedAnims.emplace(anim, AnimTime(actor, anim));
-			if (!iter.second)
-				iter.first->second = AnimTime(actor, anim);
-			animTimePtr = &iter.first->second;
+			const auto iter = g_timeTrackedAnims.emplace(anim, std::make_shared<AnimTime>(actor, anim));
+			animTimePtr = iter.first->second.get();
 		}
 		return *animTimePtr;
 	};

nvse_plugin_example/commands_animation.h
@@ -208,7 +208,7 @@ struct ActorSequenceKey
 };
 
 
-extern std::map<BSAnimGroupSequence*, AnimTime> g_timeTrackedAnims;
+extern std::map<BSAnimGroupSequence*, std::shared_ptr<AnimTime>> g_timeTrackedAnims;
 extern std::map<ActorSequenceKey, SavedAnimsTime> g_timeTrackedGroups;
 
 enum class AnimKeySetting

commit 6c09e533461232f65962d16565a70de30854ae98

Fix respectEndKey bug introduced in commit 5f178f5

nvse_plugin_example/hooks.cpp
@@ -410,9 +410,11 @@ bool __fastcall AllowAttacksEarlierInAnimHook(BaseProcess* baseProcess)
 		const auto iter = ra::find_if(g_timeTrackedAnims, [](auto& p)
 		{
 			const auto& animTime = *p.second;
+			if (animTime.allowAttackTime == -FLT_MAX)
+				return false;
 			const auto anim = animTime.anim;
 			const auto currentTime = GetAnimTime(animTime.GetAnimData(g_thePlayer), anim);
-			return animTime.allowAttackTime != -FLT_MAX && currentTime >= animTime.allowAttackTime && animTime.IsPlayer();
+			return currentTime >= animTime.allowAttackTime && animTime.IsPlayer();
 
 		});
 		if (iter != g_timeTrackedAnims.end() && GameFuncs::GetControlState(ControlCode::Attack, Decoding::IsDXKeyState::IsPressed))

nvse_plugin_example/main.cpp
@@ -133,11 +133,13 @@ void HandleAnimTimes()
 			{
 				Revert3rdPersonAnimTimes(animTime, anim);
 			};
-			if (actor->GetWeaponForm() != animTime.actorWeapon 
-				|| animTime.anim3rdCounterpart && animTime.anim3rdPlaying && g_thePlayer->Get3rdPersonAnimData()->animSequence[groupInfo->sequenceType] != animTime.anim3rdCounterpart)
+			const auto* current3rdPersonAnim = g_thePlayer->Get3rdPersonAnimData()->animSequence[groupInfo->sequenceType];
+			const auto* currentWeapon = actor->GetWeaponForm();
+			
+			if (currentWeapon != animTime.actorWeapon
+				|| !current3rdPersonAnim || !current3rdPersonAnim->animGroup
+				|| animTime.anim3rdCounterpart && animTime.anim3rdPlaying && current3rdPersonAnim->animGroup->groupID != animTime.anim3rdCounterpart->animGroup->groupID)
 			{
-				//if (!g_thePlayer->IsThirdPerson())
-				//	GameFuncs::DeactivateSequence(nullptr, animTime.anim3rdCounterpart, 0);
 				erase();
 				continue;
 			}
@@ -146,12 +148,12 @@ void HandleAnimTimes()
 				const auto sequenceId = anim->animGroup->GetGroupInfo()->sequenceType;
 				auto* anim3rd = g_thePlayer->baseProcess->GetAnimData()->animSequence[sequenceId];
 				animTime.anim3rdPlaying = anim3rd != nullptr;
-				if (!anim3rd || (anim3rd->animGroup->groupID & 0xFF) != (anim->animGroup->groupID & 0xFF))
+				if (!anim3rd || !anim3rd->animGroup || (anim3rd->animGroup->groupID & 0xFF) != (anim->animGroup->groupID & 0xFF))
 				{
 					anim3rd = anim->Get3rdPersonCounterpart();
 					animTime.anim3rdPlaying = false;
 				}
-				if (!anim3rd)
+				if (!anim3rd || !anim3rd->animGroup)
 				{
 					erase();
 					continue;